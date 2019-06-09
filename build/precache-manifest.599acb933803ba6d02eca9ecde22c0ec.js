c04c7a6047803b22f6bf1296a86164e40491608d
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2019-06-09 14:02:50 +0300

    Created production build

---
+++ build/precache-manifest.599acb933803ba6d02eca9ecde22c0ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3babc0bb105058555bd584066dda5e73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa2291061ff4c98134d",
+    "url": "/static/css/main.2e3d58ce.chunk.css"
+  },
+  {
+    "revision": "df8d1ba11ab24a96a340",
+    "url": "/static/js/2.15c16a04.chunk.js"
+  },
+  {
+    "revision": "1fa2291061ff4c98134d",
+    "url": "/static/js/main.9efa63c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
