c8868696c4bde5076b07b0935dec08dcce71ec74
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2020-01-24 19:41:08 +0300

    Add script to access database

---
+++ dbHandler.js
@@ -0,0 +1,53 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://mikhailsinyakov:' +
+						process.env.ATLAS_PASSWORD +
+						'@cluster0-6bako.mongodb.net/tetris';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let collection;
+
+const connect = () => {
+	return new Promise((resolve, reject) => {
+		client.connect(err => {
+			if (err) reject(err);
+			else {
+				collection = client.db('tetris').collection("records");
+				resolve();
+			}
+		});
+	});
+};
+
+const getRecords = async () => {
+	return new Promise((resolve, reject) => {
+		collection.find().sort({record: -1}).toArray((err, data) => {
+			if (err) reject(err);
+			else resolve(data);
+		});
+	});
+};
+
+const handleNewRecord = async newRecord => {
+	const records = await getRecords(collection);
+
+	if (records.length < 10) collection.insertOne(newRecord);
+	else if (newRecord.record > records[9].record) {
+		collection.replaceOne(
+			{username: records[9].username, record: records[9].record}, 
+			newRecord
+		);
+	}
+	return true;
+};
+
+const close = async () => {
+	await client.close();
+	return true;
+};
+
+
+module.exports = {
+	connect,
+	getRecords,
+	handleNewRecord,
+	close
+};
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+const dbHandler = require('./dbHandler');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+(async () => {
+	await dbHandler.connect();
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	});
+	
+	app.listen(process.env.PORT || 8080);
+})();
+
+process.on('SIGINT', async () => {
+	await dbHandler.close();
+  process.exit(0);
 });
-
-
-
-app.listen(process.env.PORT || 8080);
